14c251cfff5cb5177ed6b445f16e4bc5a813b677
Harsh Shelar <harshshelar22@gmail.com>
2023-11-12 23:54:22 -0500

    inital setup for all the project components

---
+++ machadolabserver/src/main/java/com/example/machadolabserver/MachadolabserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.machadolabserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachadolabserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachadolabserverApplication.class, args);
+	}
+
+}
---
+++ machadolabserver/src/test/java/com/example/machadolabserver/MachadolabserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.machadolabserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachadolabserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
